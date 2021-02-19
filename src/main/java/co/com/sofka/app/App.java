59b019dcf40c22aaff006ee20dd18b322e0e2e03
stephanie <anie.12106@gmail.com>
2021-02-18 21:32:17 -0500

    Aplique formato a las clases.

--- src/main/java/co/com/sofka/app/App.java
+++ src/main/java/co/com/sofka/app/App.java
@@ -1,39 +1,41 @@
 package co.com.sofka.app;
+
 import co.com.sofka.app.calculator.BasicCalculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 
 public class App {
-    private static final  Logger logger = LoggerFactory.getLogger( App.class );
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println( "Enter number 1: " );
+        System.out.println("Enter number 1: ");
         String textNumber1 = bufferedReader.readLine();
 
-        System.out.println( "Enter number 2: " );
+        System.out.println("Enter number 2: ");
         String textNumber2 = bufferedReader.readLine();
 
-        Long number1 = Long.valueOf( textNumber1 );
-        Long number2 = Long.valueOf( textNumber2 );
+        Long number1 = Long.valueOf(textNumber1);
+        Long number2 = Long.valueOf(textNumber2);
 
         BasicCalculator calculator = new BasicCalculator();
-        Long result = calculator.sum( number1, number2 );
-        Long resultSubstract= calculator.substract(number1,number2);
-        Long resultMultiply = calculator.multiply(number1,number2);
-        Float resultDivision = calculator.division(number1,number2);
+        Long result = calculator.sum(number1, number2);
+        Long resultSubstract = calculator.substract(number1, number2);
+        Long resultMultiply = calculator.multiply(number1, number2);
+        Float resultDivision = calculator.division(number1, number2);
 
 
-        logger.info( " {} + {} = {}", number1, number2,result);
-        logger.info( " {} - {} = {}", number1, number2,resultSubstract);
-        logger.info( " {} * {} = {}", number1, number2,resultMultiply);
-        logger.info( " {} / {} = {}", number1, number2,resultDivision);
+        logger.info(" {} + {} = {}", number1, number2, result);
+        logger.info(" {} - {} = {}", number1, number2, resultSubstract);
+        logger.info(" {} * {} = {}", number1, number2, resultMultiply);
+        logger.info(" {} / {} = {}", number1, number2, resultDivision);
 
 
     }
--- src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -8,27 +8,26 @@ public class BasicCalculator {
     private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
 
     public Long sum(Long number1, Long number2) {
-        logger.info( "Summing {} + {}", number1, number2 );
+        logger.info("Summing {} + {}", number1, number2);
         return number1 + number2;
     }
 
     public Long substract(Long number1, Long number2) {
-        logger.info( "Substraction {} - {}", number1, number2 );
+        logger.info("Substraction {} - {}", number1, number2);
         return number1 - number2;
     }
 
     public Long multiply(Long number1, Long number2) {
-        logger.info( "Multiplication {} * {}", number1, number2 );
+        logger.info("Multiplication {} * {}", number1, number2);
         return number1 * number2;
     }
 
     public Float division(Long number1, Long number2) {
-        logger.info( "Division {} / {}", number1, number2 );
-        Float resultado=0f;
+        logger.info("Division {} / {}", number1, number2);
+        Float resultado = 0f;
         try {
-            resultado= Float.valueOf(number1/number2);
-        }
-        catch (ArithmeticException e){
+            resultado = Float.valueOf(number1 / number2);
+        } catch (ArithmeticException e) {
             logger.info("La división por cero da error");
         }
         return resultado;
